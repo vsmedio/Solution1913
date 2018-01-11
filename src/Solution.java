99f59c524463f8d77f40870c72be2be0bcb62a97
Arvis Abromovics <info@vsmedio.lv>
2018-01-11 21:24:08 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,62 @@
+/* 
+Выводим только цифры
+В методе main подмени объект System.out написанной тобой ридер-оберткой по аналогии с лекцией.
+Твоя ридер-обертка должна выводить только цифры.
+Вызови готовый метод printSomething(), воспользуйтесь testString.
+Верни переменной System.out первоначальный поток.
+Выведи модифицированную строку в консоль.
+
+Пример вывода:
+12345678
+
+
+Требования:
+1. Класс Solution должен содержать класс TestString.
+2. Класс Solution должен содержать публичное статическое поле testString типа TestString, которое сразу проинициализировано.
+3. Класс TestString должен содержать публичный void метод printSomething().
+4. Метод printSomething() класса TestString должен выводить на экран строку "it's 1 a 23 text 4 f5-6or7 tes8ting".
+5. Метод main(String[] args) класса Solution должен создавать поток PrintStream (используй PrintStream c параметром конструктора ByteArrayOutputStream).
+6. Метод main(String[] args) класса Solution должен подменять и восстанавливать поток вывода в консоль объекта System.out.
+7. Метод main(String[] args) класса Solution должен вызывать метод printSomething(),объекта testString.
+8. Метод main(String[] args) класса Solution должен модифицировать строку выведенную методом printSomething() согласно заданию, и выводить её в консоль.
+
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
+*/
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class Solution {
+    public static TestString testString = new TestString();
+
+    public static void main(String[] args) {
+        PrintStream consoleStream = System.out;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream stream = new PrintStream(outputStream);
+        System.setOut(stream);
+
+        testString.printSomething ();
+
+        System.setOut(consoleStream);
+        System.out.println(outputStream.toString().replaceAll("\\D+",""));
+
+    }
+
+    public static class TestString {
+        public void printSomething() {
+            System.out.println("it's 1 a 23 text 4 f5-6or7 tes8ting");
+        }
+    }
+}
